3c9da1c426e1016df7c3a8ec70029d13372204b0
9102129030 <144200202+9102129030@users.noreply.github.com>
2024-08-12 18:24:43 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import AllRoutes from './Routs/Allroutes'
+import AllRoutes from './Routs/AllRoutes'
 import Navbar from './Components/Navbar'
 
 function App() {
@@ -10,7 +8,7 @@ function App() {
 
   return (
     <>
-    <Navbar/>
+     <Navbar/>
      <AllRoutes/>
     </>
   )
